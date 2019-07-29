12eadee1d2afab6156ad18a5660dfcfe0bddcfc6
shi.rei <shi.rei@vega-c.com>
2019-07-29 16:41:55 +0900

    add firebase

---
+++ app/src/main/java/com/ls/dareman/DMFirebaseMessagingService.java
@@ -0,0 +1,48 @@
+package com.ls.dareman;
+
+import android.util.Log;
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+import java.util.Map;
+
+public class DMFirebaseMessagingService extends FirebaseMessagingService {
+    private static String TAG = "DMFirebaseMessagingService";
+
+
+    /**
+     * Called if InstanceID token is updated. This may occur if the security of
+     * the previous token had been compromised. Note that this is called when the InstanceID token
+     * is initially generated so this is where you would retrieve the token.
+     */
+    @Override
+    public void onNewToken(String token) {
+        Log.d(TAG, "Refreshed token: " + token);
+
+        // If you want to send messages to this application instance or
+        // manage this apps subscriptions on the server side, send the
+        // Instance ID token to your app server.
+//        sendRegistrationToServer(token);
+    }
+
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+
+        if (remoteMessage != null) {
+            Log.d(TAG, "From: " + remoteMessage.getFrom());
+
+            RemoteMessage.Notification notification = remoteMessage.getNotification();
+            if (notification != null) {
+                Log.d(TAG, "notification: " + notification);
+            }
+
+            Map<String, String> data = remoteMessage.getData();
+            if (data != null) {
+                Log.d(TAG, "data: " + data);
+            }
+        }
+
+        // Also if you intend on generating your own notifications as a result of a received FCM
+        // message, here is where that should be initiated. See sendNotification method below.
+    }
+}
--- app/src/main/java/com/ls/dareman/SplashActivity.java
+++ app/src/main/java/com/ls/dareman/SplashActivity.java
@@ -1,14 +1,30 @@
 package com.ls.dareman;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.FragmentActivity;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.analytics.FirebaseAnalytics;
+import com.google.firebase.dynamiclinks.FirebaseDynamicLinks;
+import com.google.firebase.dynamiclinks.PendingDynamicLinkData;
+import com.google.firebase.iid.FirebaseInstanceId;
 
 
 public class SplashActivity extends FragmentActivity {
+    private FirebaseAnalytics mFirebaseAnalytics;
 
-    public static final String[] DEBUG_HOST_URL = {
+    public static final String[] HOST_URL = {
             "https://dokodemo.world/",
             "https://mintj.com/?mdc=991&afguid=tx0icnka6n4i20rfvwa3guaos",
             "https://st-sub.dokodemo.world/",
@@ -18,7 +34,7 @@ public class SplashActivity extends FragmentActivity {
             "https://google.com/"
     };
 
-    public static final String[] DEBUG_ITEMS = {
+    public static final String[] ITEMS = {
             "ワクワク",
             "Jメール",
             "メルパラ",
@@ -31,23 +47,105 @@ public class SplashActivity extends FragmentActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
+        setChannel();
+
+//        FirebaseInstanceId.getInstance().getInstanceId().addOnCompleteListener((task) -> {
+//            if (!task.isSuccessful()) {
+//                Log.w("FIREBASE", "getInstanceId failed.", task.getException());
+//                return;
+//            }
+//
+//            String token = (task.getResult() == null) ? "empty" : task.getResult().getToken();
+//            Log.i("FIREBASE", "[CALLBACK] Token = " + token);
+//        });
+
         onDebugSelector();
+
+        getDynamicLinks();
     }
 
     public void onDebugSelector() {
         String title = "Please Select Site";
-        DialogFragmentHelper.listDialog(getSupportFragmentManager(), title, DEBUG_ITEMS, new DialogResultListener<Integer>() {
+        DialogFragmentHelper.listDialog(getSupportFragmentManager(), title, ITEMS, new DialogResultListener<Integer>() {
             @Override
             public void onDataResult(Integer result) {
 
-                String loadUrl = DEBUG_HOST_URL[result];
+                String loadUrl = HOST_URL[result];
                 Intent intent = new Intent(SplashActivity.this, MainActivity.class);
                 intent.putExtra("id", result);
                 intent.putExtra("url", loadUrl);
+
+                Bundle bundle = new Bundle();
+                bundle.putString(FirebaseAnalytics.Param.ITEM_ID, String.valueOf(result));
+                bundle.putString(FirebaseAnalytics.Param.ITEM_NAME, ITEMS[result]);
+                bundle.putString(FirebaseAnalytics.Param.CONTENT_TYPE, "url");
+                mFirebaseAnalytics.logEvent(FirebaseAnalytics.Event.SELECT_CONTENT, bundle);
+
                 startActivity(intent);
                 finish();
 
             }
         }, false);
+    }
+
+    private void setChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationManager manager = (NotificationManager)getSystemService(Context.NOTIFICATION_SERVICE);
+
+            NotificationChannel channel = new NotificationChannel(
+                    // 一意のチャンネルID
+                    // ここはどこかで定数にしておくのが良さそう
+                    "dm_push",
+
+                    // 設定に表示されるチャンネル名
+                    // ここは実際にはリソースを指定するのが良さそう
+                    "DMからのプッシュ通知",
+
+                    // チャンネルの重要度
+                    // 重要度によって表示箇所が異なる
+                    NotificationManager.IMPORTANCE_DEFAULT
+            );
+
+            // 通知時にライトを有効にする
+            channel.enableLights(true);
+            // 通知時のライトの色
+            channel.setLightColor(Color.WHITE);
+            // ロック画面での表示レベル
+            channel.setLockscreenVisibility(Notification.VISIBILITY_PUBLIC);
+
+            // チャンネルの登録
+            manager.createNotificationChannel(channel);
+        }
+    }
+
+    public void getDynamicLinks() {
+        FirebaseDynamicLinks.getInstance()
+                .getDynamicLink(getIntent())
+                .addOnSuccessListener(this, new OnSuccessListener<PendingDynamicLinkData>() {
+                    @Override
+                    public void onSuccess(PendingDynamicLinkData pendingDynamicLinkData) {
+                        // Get deep link from result (may be null if no link is found)
+                        Uri deepLink = null;
+                        if (pendingDynamicLinkData != null) {
+                            deepLink = pendingDynamicLinkData.getLink();
+                            Log.i("DYNAMIC-LINKS", deepLink.toString());
+                        }
+
+
+                        // Handle the deep link. For example, open the linked
+                        // content, or apply promotional credit to the user's
+                        // account.
+                        // ...
+
+                        // ...
+                    }
+                })
+                .addOnFailureListener(this, new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.w("DYNAMIC-LINKS", "getDynamicLink:onFailure", e);
+                    }
+                });
     }
 }
