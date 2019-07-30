53de0a7be44389b15bd7a55e358e760b65337ea4
shi.rei <shi.rei@vega-c.com>
2019-07-30 19:05:17 +0900

    ui fix-2

--- app/src/main/java/com/ls/dareman/DashFragment.kt
+++ app/src/main/java/com/ls/dareman/DashFragment.kt
@@ -1,12 +1,16 @@
 package com.ls.dareman
 
+import android.R
 import android.content.Intent
 import android.os.Bundle
+import android.util.TypedValue
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.Toast
+import androidx.constraintlayout.widget.ConstraintLayout
 import com.google.android.gms.ads.AdListener
 import com.google.android.gms.ads.AdRequest
 import com.google.android.gms.ads.InterstitialAd
@@ -19,6 +23,11 @@ class DashFragment: DMFragment() {
         super.onCreateView(inflater, container, savedInstanceState)
 
         val v = inflater.inflate(com.ls.dareman.R.layout.fragment_menu, container, false)
+
+//        val constraintLayout = v.findViewById(com.ls.dareman.R.id.constraintLayout) as ConstraintLayout
+//        val img = v.findViewById(com.ls.dareman.R.id.title_img) as ImageView
+//        constraintLayout.addView(img)
+
         initInterstitialAds()
 
         return v
@@ -28,6 +37,8 @@ class DashFragment: DMFragment() {
         super.onActivityCreated(savedInstanceState)
 
         val buttonOne = activity!!.findViewById(com.ls.dareman.R.id.menu_one) as Button
+        buttonOne.setTextSize(TypedValue.COMPLEX_UNIT_PX, 36F)
+
         buttonOne.setOnClickListener {
             Toast.makeText(activity, "hoge!", Toast.LENGTH_SHORT).show()
             val intent = Intent(activity, SplashActivity::class.java)
@@ -39,6 +50,8 @@ class DashFragment: DMFragment() {
         }
 
         val buttonTwo = activity!!.findViewById(com.ls.dareman.R.id.menu_two) as Button
+        buttonTwo.setTextSize(TypedValue.COMPLEX_UNIT_PX, 36F)
+
         buttonTwo.setOnClickListener {
             Toast.makeText(activity, "Contents preparing...", Toast.LENGTH_SHORT).show()
 
@@ -53,7 +66,7 @@ class DashFragment: DMFragment() {
         // debug
 //        mInterstitialAd.adUnitId = "ca-app-pub-3940256099942544/1033173712"
         // prod
-        mInterstitialAd.adUnitId = getString(R.string.interstitial_ad_unit_id)
+        mInterstitialAd.adUnitId = getString(com.ls.dareman.R.string.interstitial_ad_unit_id)
         mInterstitialAd.loadAd(AdRequest.Builder().build())
 
         mInterstitialAd.adListener = object: AdListener() {
